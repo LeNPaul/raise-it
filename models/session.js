2da2619d4e65f59a91a58939bcfc4861b8b8a3f3
Paul Le <l.nguyen.paul@gmail.com>
2020-05-20 23:57:07 -0400

    #17 Update database schema for maintaining a session to include session_description, session_location, and session_url

--- models/session.js
+++ models/session.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 const Sessions = new Schema({
   session_id: String,
   session_name: String,
+  session_description: String,
+  session_location: String,
+  session_url: String,
   host_name: String,
   state_date_time: Date,
   end_date_time: Date
