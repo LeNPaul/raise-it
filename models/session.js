11aff0e89c529255cf6dcae91622ae2e381e2971
Paul Le <l.nguyen.paul@gmail.com>
2020-05-21 00:35:09 -0400

    #17 Remove session_location and session_url, fix typo for start_date_time, and add host_contact_info for session schema

--- models/session.js
+++ models/session.js
@@ -5,10 +5,9 @@ const Sessions = new Schema({
   session_id: String,
   session_name: String,
   session_description: String,
-  session_location: String,
-  session_url: String,
   host_name: String,
-  state_date_time: Date,
+  host_contact_info: String,
+  start_date_time: Date,
   end_date_time: Date
 });
 
