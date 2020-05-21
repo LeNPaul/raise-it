0a54e31dc697c90e00a408e9fb36f3c33f49c182
Paul Le <l.nguyen.paul@gmail.com>
2020-05-21 15:40:09 -0400

    #18 Add route for starting session through /session/start/:session_id

--- routes/session.js
+++ routes/session.js
@@ -6,4 +6,8 @@ router.get('/start', function(req, res, next) {
   res.render('start', { user : req.user });
 });
 
+router.post('/start/:session_id', (req, res) => {
+  res.json({hello: 'world!', session_id: req.params.session_id});
+});
+
 module.exports = router;
