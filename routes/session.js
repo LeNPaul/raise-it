b84bddd85c4cc54dd253f6e3de47e067680fed10
Paul Le <l.nguyen.paul@gmail.com>
2020-05-21 22:32:20 -0400

    #18 Update /session/start endpoint to accept session information for starting a new session

--- routes/session.js
+++ routes/session.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET page for starting session. */
-router.get('/start', function(req, res, next) {
+router.get('/start', function(req, res) {
   res.render('start', { user : req.user });
 });
 
-router.post('/start/:session_id', (req, res) => {
-  res.json({hello: 'world!', session_id: req.params.session_id});
+/* POST request for starting session */
+router.post('/start', (req, res, next) => {
+  res.json({
+    session_name: req.body.session_name,
+    session_description: req.body.session_description,
+    host_name: req.body.host_name,
+    host_contact_info: req.body.host_contact_info
+  });
 });
 
 module.exports = router;
