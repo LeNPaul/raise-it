069d28ddb7687c08638a30dfdde4c4391011ad1b
Paul Le <l.nguyen.paul@gmail.com>
2020-05-22 00:11:42 -0400

    #18 Update to set session_id to a GUID

--- routes/session.js
+++ routes/session.js
@@ -10,18 +10,22 @@ router.get('/start', function(req, res) {
 /* POST request for starting session */
 // curl --header "Content-Type: application/json" --data '{"session_id":"asdf", "session_name": "asdfasfdsf"}' localhost:3000/session/start
 router.post('/start', (req, res) => {
-
+  // https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+  function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
   var newSession = new Session({
-    session_id: '<generate session id>',
+    session_id: uuidv4(),
     session_name: req.body.session_name,
     session_description: req.body.session_description,
     host_name: req.body.host_name,
     host_contact_info: req.body.host_contact_info,
     start_date_time: new Date()
   })
-
   newSession.save(function(err, data) {
-    console.log(err);
     if (err) {res.json({success: false})} else {
       res.json({
         session_name: req.body.session_name,
@@ -31,7 +35,6 @@ router.post('/start', (req, res) => {
       });
     };
   })
-
 });
 
 module.exports = router;
