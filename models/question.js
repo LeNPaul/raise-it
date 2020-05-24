c47f50fe2453f850c8c12061cc60862c0ee543d2
Paul Le <l.nguyen.paul@gmail.com>
2020-05-24 18:32:41 -0400

    #22 Update database schema for questions to include parameters for upvotes, is_answered, and submitted_date_time

--- models/question.js
+++ models/question.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Question = new Schema({
+  session_id: String,
   question_id: String,
   question_text: String,
-  session_id: String
+  upvotes: Integer,
+  is_answered: Boolean,
+  submitted_date_time: Date
 });
 
 module.exports = mongoose.model('question', Question);
