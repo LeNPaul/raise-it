1e97a37df4e343bde75d56746bf2950bd654df8d
Paul Le <l.nguyen.paul@gmail.com>
2020-05-30 01:16:08 -0400

    #20 Make API call to get audience questions for a session and display on the session page

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -1,15 +1,14 @@
 var sessionApp = new Vue({
   el: '#sessionApp',
   data: {
-    questions: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
+    questions: null
   },
   methods: {
     getQuestions: function(session_id) {
       axios
-        .get('/session/questions/' + session_id).then(response => console.log(response.data))
+        .get('/session/questions/' + session_id).then(response =>  this.questions = response.data);
     }
   }
 })
+
+sessionApp.getQuestions('1234')
