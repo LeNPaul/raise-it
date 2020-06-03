61b3673c06f9a2e53048370869c162dde13373a3
Paul Le <l.nguyen.paul@gmail.com>
2020-06-02 23:44:46 -0400

    #21 Use window.setInterval() to regularly poll a getQuestions function

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -4,12 +4,17 @@ var sessionApp = new Vue({
     info: null,
     questions: null
   },
+  created () {
+    this.getQuestions();
+    this.timer = setInterval(this.getQuestions, 3000)
+  },
   methods: {
     getInfo: function(session_id) {
       axios
         .get('/session/data/' + session_id).then(response => this.info = response.data);
     },
     getQuestions: function(session_id) {
+      console.log(session_id);
       axios
         .get('/session/questions/' + session_id).then(response =>  this.questions = response.data);
     }
