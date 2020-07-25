c8f4cfd733eab833458ba0de24aae2185534ede1
Paul Le <l.nguyen.paul@gmail.com>
2020-07-25 16:15:48 -0400

    #13 Add method to redirect audience to audience view of session page

--- public/javascripts/start.js
+++ public/javascripts/start.js
@@ -10,8 +10,11 @@ var formApp = new Vue({
     startSession: function() {
       axios
         .post('/session/start', {session_name: this.sessionName, session_description: this.sessionDescription, host_name: this.hostName, host_contact_info: this.hostContact}).then(response => {
-          window.location.href = "/session/presenter#/id/" + response.data.session_id;
+          window.location.href = '/session/presenter#/id/' + response.data.session_id;
         });
+    },
+    joinSession: function() {
+      window.location.href = '/session/audience#/id/' + this.sessionId
     }
   }
 })
