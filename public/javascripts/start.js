259b3a45df705de45174e90803dce197ee9a475a
Paul Le <l.nguyen.paul@gmail.com>
2020-07-25 16:05:01 -0400

    #13 Redirect to presenter view of session page when a new session is started

--- public/javascripts/start.js
+++ public/javascripts/start.js
@@ -9,7 +9,9 @@ var formApp = new Vue({
   methods: {
     startSession: function() {
       axios
-        .post('/session/start', {session_name: this.sessionName, session_description: this.sessionDescription, host_name: this.hostName, host_contact_info: this.hostContact}).then(response => console.log(response.data))
+        .post('/session/start', {session_name: this.sessionName, session_description: this.sessionDescription, host_name: this.hostName, host_contact_info: this.hostContact}).then(response => {
+          window.location.href = "/session/presenter#/id/" + response.data.session_id;
+        });
     }
   }
 })
