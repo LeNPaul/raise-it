d7322911aaac64241d964ee4dba6494c2a3eaf45
Paul Le <l.nguyen.paul@gmail.com>
2020-07-26 13:24:38 -0400

    #29 Create route for submitting a question to an active session and save the question in the database

--- routes/session.js
+++ routes/session.js
@@ -3,6 +3,14 @@ var router = express.Router();
 var Session = require('../models/session');
 var Question = require('../models/question')
 
+// https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 /* GET page for starting session. */
 router.get('/start', function(req, res) {
   res.render('start', { user : req.user });
@@ -11,13 +19,6 @@ router.get('/start', function(req, res) {
 /* POST request for starting session */
 // curl --header "Content-Type: application/json" --data '{"session_id":"asdf", "session_name": "asdfasfdsf"}' localhost:3000/session/start
 router.post('/start', (req, res) => {
-  // https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
-  function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
   var session_id = uuidv4();
   var newSession = new Session({
     session_id: session_id,
@@ -120,13 +121,14 @@ router.post('/question', function(req, res) {
   })
 })
 
-/* TEST endpoint to create new question */
-router.get('/create_question', function(req, res) {
+/* POST create a new question */
+// curl --header "Content-Type: application/json" --data '{"session_id": "b3b3d326-fb14-46ed-9d0e-2a0a094f5bb0", "question_text": "Hello, its me again, I guess I can not use apostrophes"}' http://localhost:8080/session/question/new
+router.post('/question/new', function(req, res) {
   // Create new question
   var newQuestion = new Question({
-    session_id: '1234',
-    question_id: '1234',
-    question_text: 'This is a new placeholder question text',
+    session_id: req.body.session_id,
+    question_id: uuidv4(),
+    question_text: req.body.question_text,
     upvotes: 0,
     is_answered: false,
     submitted_date_time: new Date()
