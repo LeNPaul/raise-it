5111dff479c4ca4119d1be2b09acd8a8e1f573ba
Paul Le <l.nguyen.paul@gmail.com>
2020-08-01 12:21:57 -0400

    #36 Pass username when logged in when making request to /session/start endpoint

--- public/javascripts/start.js
+++ public/javascripts/start.js
@@ -9,6 +9,10 @@ var formApp = new Vue({
   },
   methods: {
     startSession: function() {
+      var user = this.$refs.user;
+      if (user) {
+        this.userName = user.getAttribute('username');
+      }
       axios
         .post('/session/start', {session_name: this.sessionName, session_description: this.sessionDescription, user_name: this.userName, host_name: this.hostName, host_contact_info: this.hostContact}).then(response => {
           window.location.href = '/session/presenter#/id/' + response.data.session_id;
