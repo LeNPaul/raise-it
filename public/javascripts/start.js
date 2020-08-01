64f6dae14dd70613c8963ff221e494e8d85981c9
Paul Le <l.nguyen.paul@gmail.com>
2020-08-01 11:37:44 -0400

    #36 Update formApp to pass userName in request to /session/start endpoint

--- public/javascripts/start.js
+++ public/javascripts/start.js
@@ -3,13 +3,14 @@ var formApp = new Vue({
   data: {
     sessionName: null,
     sessionDescription: null,
+    userName: null,
     hostName: null,
     hostContact: null,
   },
   methods: {
     startSession: function() {
       axios
-        .post('/session/start', {session_name: this.sessionName, session_description: this.sessionDescription, host_name: this.hostName, host_contact_info: this.hostContact}).then(response => {
+        .post('/session/start', {session_name: this.sessionName, session_description: this.sessionDescription, user_name: this.userName, host_name: this.hostName, host_contact_info: this.hostContact}).then(response => {
           window.location.href = '/session/presenter#/id/' + response.data.session_id;
         });
     },
