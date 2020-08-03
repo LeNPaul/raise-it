ca230285fad7ddf20b56d0e995ff26016d2a44b1
Paul Le <l.nguyen.paul@gmail.com>
2020-08-03 19:12:15 -0400

    #38 Add script for setting up mongodb

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# Set up user for running application
 sudo adduser raiseit
 sudo usermod -aG sudo raiseit
 sudo ufw allow OpenSSH
 sudo ufw enable
+
+# Set up MongoDB
+sudo apt update
+sudo apt install -y mongodb
