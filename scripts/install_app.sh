1dd1bf43af17bad3198f99f5715721044cb1e3b1
Paul Le <l.nguyen.paul@gmail.com>
2020-08-03 19:20:46 -0400

    #38 Add script for setting up Node.js environment

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -6,6 +6,18 @@ sudo usermod -aG sudo raiseit
 sudo ufw allow OpenSSH
 sudo ufw enable
 
-# Set up MongoDB
+# Update package manager
 sudo apt update
+
+# Set up Git
+sudo apt install -y git
+
+# Set up MongoDB
 sudo apt install -y mongodb
+
+# Set up Node.js
+sudo apt install -y nodejs
+sudo apt install -y npm
+
+# Set up PM2
+sudo npm install pm2@latest -g
