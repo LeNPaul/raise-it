25c8bac16e3c9445148ac3af13c340508eb93778
Paul Le <l.nguyen.paul@gmail.com>
2020-08-08 22:56:51 -0400

    #38 Update script for setting up nginx and setting up Let's Encrypt

--- scripts/install/configure_server.sh
+++ scripts/install/configure_server.sh
@@ -5,8 +5,8 @@ sudo apt update
 
 # Set up nginx
 sudo apt install -y nginx
-sudo systemctl start nginx
 sudo systemctl enable nginx
+sudo systemctl start nginx
 
 # Update firewall
 sudo ufw allow 'Nginx HTTPS'
@@ -15,6 +15,17 @@ sudo ufw allow 'Nginx HTTPS'
 sudo cp ../nginx/raiseit.lenpaul.com /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/raiseit.lenpaul.com /etc/nginx/sites-enabled/
 
+# To avoid a possible hash bucket memory problem that can arise from adding additional server names
+sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/g' /etc/nginx/nginx.conf
+sudo systemctl restart nginx
+
 # Set up Let's Encrypt
-# sudo add-apt-repository ppa:certbot/certbot
-# sudo apt install python-certbot-nginx
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y certbot python3-certbot-nginx
+
+# Configure the certificate (nginx already configured to read certificate file)
+sudo certbot certonly --nginx
--- scripts/nginx/raiseit.lenpaul.com
+++ scripts/nginx/raiseit.lenpaul.com
@@ -13,16 +13,15 @@ server {
         proxy_cache_bypass $http_upgrade;
     }
 
-    listen 443 ssl;
-    ssl_certificate /etc/letsencrypt/live/raiseit.lenpaul.com/fullchain.pem; # managed by Certbot
-    ssl_certificate_key /etc/letsencrypt/live/raiseit.lenpaul.com/privkey.pem; # managed by Certbot
+    #listen 443 ssl;
+    #ssl_certificate /etc/letsencrypt/live/raiseit.lenpaul.com/fullchain.pem; # managed by Certbot
+    #ssl_certificate_key /etc/letsencrypt/live/raiseit.lenpaul.com/privkey.pem; # managed by Certbot
 
-    include /etc/letsencrypt/options-ssl-nginx.conf;
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+    #include /etc/letsencrypt/options-ssl-nginx.conf;
+    #ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
-    if ($scheme != "https") {
-        return 301 https://$host$request_uri;
-    }
-
+    #if ($scheme != "https") {
+    #    return 301 https://$host$request_uri;
+    #}
 
 }
